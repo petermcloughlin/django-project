e710f58a9be1509b341a138008386376448dc46c
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-09-11 08:21:20 +0000

    Add whitenoise package installation to project

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gf(10x9+$8@2degg078*yn$i4)xv7_sm2^l!1y_+yvahhd009b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-petermcloug-djangoproje-yy7jaou5ji0.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
